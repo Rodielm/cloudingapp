2fafe136f1d0a3ade23e1f5f104176dbb68f8de5
Rodielmj <rodielmj@gmail.com>
2019-06-12 01:54:59 +0200

    start new one

---
+++ clouding-api/src/main/java/es/uv/twcam/cloudingapi/CloudingAirApp.java
@@ -0,0 +1,13 @@
+package es.uv.twcam.cloudingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudingAirApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudingAirApp.class, args);
+	}
+
+}
---
+++ clouding-reactive/src/main/java/es/uv/twcam/cloudingreactive/CloudingAirReactiveApp.java
@@ -0,0 +1,13 @@
+package es.uv.twcam.cloudingreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudingAirReactiveApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudingAirReactiveApp.class, args);
+	}
+
+}
