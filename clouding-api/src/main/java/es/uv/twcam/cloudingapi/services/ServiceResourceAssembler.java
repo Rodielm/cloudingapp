cbdb8ad30722ace56f971ada27a1a3b6656869b3
rodielm <rmartinezj@alfatecsistemas.es>
2019-06-13 14:24:24 +0200

    added resoruce

---
+++ clouding-api/src/main/java/es/uv/twcam/cloudingapi/services/ServiceResourceAssembler.java
@@ -0,0 +1,13 @@
+package es.uv.twcam.cloudingapi.services;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * ServiceResourceAssembler
+ */
+
+@Component
+public class ServiceResourceAssembler   {
+
+    
+}
--- clouding-api/src/main/java/es/uv/twcam/cloudingapi/test.java
+++ clouding-api/src/main/java/es/uv/twcam/cloudingapi/test.java
@@ -1,7 +1,5 @@
 package es.uv.twcam.cloudingapi;
 
-import java.sql.Date;
-import java.util.Calendar;
 
 /**
  * test
@@ -9,23 +7,7 @@ import java.util.Calendar;
 public class test {
     public static void main(String[] args) {
 
-        Calendar c = Calendar.getInstance();
-        c.getTime();
-       
-        try {
-            System.out.println(c.getTime());
-            System.out.println(c.getTime());
-            System.out.println(c.getTime());
-            System.out.println(c.getTime());
-            Thread.sleep(5000);
-            System.out.println(c.getTime());
-  System.out.println(c.getTime());System.out.println(c.getTime());
-  System.out.println(c.getTime());
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-  
-
+     
+     
 }
 }
