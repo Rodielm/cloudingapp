ac62fb534f65a2348691c81c8b2bab65c9dd2231
Rodielmj <rodielmj@gmail.com>
2019-06-16 18:09:19 +0200

    changed version spring boot

--- clouding-api/src/main/java/es/uv/twcam/cloudingapi/services/assembler/ServiceResourceAssembler.java
+++ clouding-api/src/main/java/es/uv/twcam/cloudingapi/services/assembler/ServiceResourceAssembler.java
@@ -1,12 +1,25 @@
 package es.uv.twcam.cloudingapi.services.assembler;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceAssembler;
+import org.springframework.stereotype.Component;
+
+import es.uv.twcam.cloudingapi.entities.Airplane;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
 /**
  * ServiceResourceAssembler
  */
 
+@Component
+public class ServiceResourceAssembler implements ResourceAssembler<Airplane, Resource<Airplane>> {
 
-public class ServiceResourceAssembler  {
+    @Override
+    public Resource<Airplane> toResource(Airplane entity) {
+        return null;
+    }
 
-    
 }
