329ea26e61e92bf5f962cae824df9ce301531b05
rodielm <rmartinezj@alfatecsistemas.es>
2019-06-17 11:15:52 +0200

    added AirportControlCtrl

---
+++ clouding-reactive/src/main/java/es/uv/twcam/cloudingreactive/api/AirportControlCtrl.java
@@ -0,0 +1,14 @@
+package es.uv.twcam.cloudingreactive.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * AirportControlCtrl
+ */
+@RestController
+@RequestMapping("/api/airportcontrol")
+public class AirportControlCtrl {
+
+    
+}
